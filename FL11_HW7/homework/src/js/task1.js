0deeb85644820847bf8162deb54873267cf82c69
Nazarkon <nazarmontsibovych@gmail.com>
2019-07-05 18:09:29 +0300

    Version 1.0 First task

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,38 +1,51 @@
-let validateAdminEmail = 'user@gmail.com' , validateUserEmail ='admin@gmail.com';
-let correctUserPassword = 'UserPass' , correctAdminPassword = 'AdminPass';
+let validateAdminEmail = 'user@gmail.com', validateUserEmail = 'admin@gmail.com';
+let correctUserPassword = 'UserPass', correctAdminPassword = 'AdminPass';
 let minimumLength = 6;
+let minimumPasswordLength = 5;
 
-let validationLogin = prompt('Enter your Email','');
+let validationLogin = prompt('Enter your Email', '');
 
-if(validationLogin === validateAdminEmail || validationLogin === validateUserEmail){
+if (validationLogin === validateAdminEmail || validationLogin === validateUserEmail) {
     let validationPassword = prompt('Enter your Password', '');
 
-    if(validationPassword === correctUserPassword || validationPassword === correctUserPassword){
-        alert('Correct password');
-               
-            let changePassword = confirm('Do you want to change your password?')
-              
-             if(changePassword === true){
+    if (validationPassword === correctUserPassword || validationPassword === correctAdminPassword) {
 
-                if(validationPassword === correctUserPassword || validationPassword === correctUserPassword){
-                    alert('Correct password');
-                }else if(validationPassword === null || validationPassword === ''){
+        let changePassword = confirm('Do you want to change your password?')
+
+        if (changePassword === true) {
+
+            validationPassword = prompt('Enter your Password', '');
+
+            if (validationPassword === correctUserPassword || validationPassword === correctAdminPassword) {
+
+                let newPassword = prompt('Enter new password', '');
+
+                if (newPassword.length < minimumPasswordLength) {
+
+                    alert('It’s too short password. Sorry.');
+
+                } else if (newPassword === null || newPassword === '') {
+
                     alert('Canceled');
                 }else{
-                    alert('Wrong password');
+                    alert('You wrote the wrong Password.');
                 }
-             }else if(changePassword === false){
-                 alert('You have failed the change');
-             }
-    }else if(validationPassword === null || validationPassword === ''){
-        alert('Canceled');
-    }else{
-        alert('Wrong password');
+            } else if (validationPassword === null || validationPassword === '') {
+
+                alert('Canceled');
+            } else {
+                alert('Wrong password');
+            }
+        } else if (changePassword === false) {
+            alert('You have failed the change');
+        }
+    } else {
+        alert('Wrong Password');
     }
-    }else if(validationLogin === null || validationLogin === undefined){
-        alert('Canceled');
-    }else if(validationLogin.length < minimumLength){
-        alert("I don't know any emails having name length less than 6 symbols");
-    }else{
-        alert('I don’t know you');
-    }
+} else if (validationLogin === null || validationLogin === '') {
+    alert('Canceled');
+} else if (validationLogin.length < minimumLength) {
+    alert("I don't know any emails having name length less than 6 symbols");
+} else {
+    alert('I don’t know you');
+}
