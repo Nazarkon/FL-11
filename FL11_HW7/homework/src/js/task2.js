2c52ba1ed95d0fe0e294391d7a206a2e4f29882e
Nazarkon <nazarmontsibovych@gmail.com>
2019-07-09 14:53:14 +0300

    Add 7.2 Version 1.2

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -13,34 +13,34 @@ if (startGame === true) {
         let userPrediction = +prompt(
             `Choose a roulette pocket number from 0 to ${maxNumber}
             Attempts left: ${tentative}
-            Total prize:${finalSumma}$;
+            Total prize:${finalSumma}$
             Possible prize on current attempt:${maxSumma}`);
         if (userPrediction === randomize) {
             currentSumma = currentSumma + maxSumma;
             finalSumma = currentSumma;
             alert(`Congratulation, you won!   Your prize is: ${finalSumma}$.`);
-            while (tentative < 1) {
+            if (tentative > 1) {
                 startGame = confirm('Do you want to continue?');
+            } else {
+                break;
             }
-                if (startGame === true) {
-                    maxNumber = maxNumber + additionRandom;
-                    maxSumma = maxSumma * multiplierSummu;
-                } else if (startGame === false) {
-                    alert(`Thank you for your participation. Your prize is:${finalSumma}`);
-                }
-            
-            continue;
-        } else if (userPrediction !== randomize) {
+            if (startGame === true) {
+                maxNumber = maxNumber + additionRandom;
+                maxSumma = maxSumma * multiplierSummu;
+            } else if (startGame === false) {
+                alert(`Thank you for your participation. Your prize is:${finalSumma}`);
+                break;
+            }
+        } else if (userPrediction !== randomize || null) {
             alert(`Thank you for your participation. Your prize is:${finalSumma}`);
             maxSumma = maxSumma / multiplierSummu;
             while (tentative < 1) {
                 startGame = confirm('Do you want to continue?');
             }
-                if (startGame === false) {
-                    alert(`Thank you for your participation. Your prize is:${finalSumma}`);
-                }
-            
-            continue;
+            if (startGame === false) {
+                alert(`Thank you for your participation. Your prize is:${finalSumma}`);
+                break;
+            }
         }
     }
 } else {
