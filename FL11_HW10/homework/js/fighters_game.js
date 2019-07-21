965fcfb0b24a3c19e8475042be8daa09034fd80f
Nazarkon <nazarmontsibovych@gmail.com>
2019-07-21 19:15:10 +0300

    Version 2.1

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,11 +1,11 @@
-let maxHp = 100;
-let minHp = 0;
-let wins = 0;
-let lose = 0;
-let currhp = 100;
 class Fighter {
-    constructor(object) {
 
+    constructor(object) {
+        let maxHp = 100;
+        let minHp = 0;
+        let wins = 0;
+        let lose = 0;
+        let currhp = object.hp;
         this.getName = () => object.name,
             this.getDamage = () => object.damage,
             this.getAgility = () => object.agility,
@@ -16,20 +16,27 @@ class Fighter {
             this.addLose = function () {
                 lose += 1;
             }
+            this.heal = function (healpoints) {
+                if (currhp >= maxHp) {
+                    currhp = maxHp;
+                }else{
+                    currhp +=healpoints;
+                }
+            }
         this.attack = (fighterAttack) => {
             let interest = 100;
             let between = interest - fighterAttack.getAgility();
             if (Math.random() * interest < between) {
                 let damageValue = object.damage;
-                this.dealDamage(damageValue);
-                return console.log(this.getName() + ' give ' + this.getDamage() + ' damage ');
+            console.log(this.getName() + ' give ' + this.getDamage() + ' damage ' + 'to ' + fighterAttack.getName());
+                Fighter2.dealDamage(damageValue);
             } else {
-                return console.log('missed');
+                console.log(this.getName() + ' missed');
             }
         }
         this.dealDamage = function (damagevl) {
 
-            if (currhp <= minHp) {
+            if (this.gethp() <= minHp) {
                 currhp = minHp;
             } else {
                 currhp -= damagevl
@@ -44,11 +51,11 @@ let battle = (Fighter1, Fighter2) => {
     Fighter1.attack(Fighter2);
     Fighter2.attack(Fighter1);
     if (Fighter1.gethp() === 0) {
-        Fighter1.addWin();
+        Fighter2.addWin();
         Fighter1.addLose();
         console.log(`You lose ${Fighter1.getName()}`);
     } else if (Fighter2.gethp() === 0) {
-        Fighter2.addWin();
+        Fighter1.addWin();
         Fighter2.addLose();
         console.log(`You lose ${Fighter2.getName()}`);
     } else {
@@ -57,14 +64,14 @@ let battle = (Fighter1, Fighter2) => {
 }
 const Fighter1 = new Fighter({
     name: 'John',
-    damage: 40,
-    agility: 25,
+    damage: 20,
+    agility: 70,
     hp: 100
 });
 const Fighter2 = new Fighter({
     name: 'Jim',
-    damage: 20,
+    damage: 50,
     agility: 5,
-    hp: 100
+    hp: 120
 });
 battle(Fighter1, Fighter2);
