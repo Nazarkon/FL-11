a54118dab734caf9c08766f0a18e15cb1c2aaff9
Nazarkon <nazarmontsibovych@gmail.com>
2019-08-29 01:17:18 +0300

    Work template

---
+++ FL11_19_homework_tools/src/js/index.js
@@ -0,0 +1,8 @@
+import './rainbow.js';
+console.log('Hello!')
+console.log("I am here!")
+
+let newel = (greet) => {
+    return greet;
+}
+console.log(newel("hello"));
---
+++ FL11_19_homework_tools/src/js/rainbow.js
@@ -0,0 +1,3 @@
+let a = 5 ;
+let b = 4;
+console.log(a+b)
