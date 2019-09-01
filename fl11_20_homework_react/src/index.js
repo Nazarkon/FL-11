d795c8218d1ad30321da37a24c2bfeb49f53a7d4
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-02 00:35:54 +0300

    Get started

--- fl11_20_homework_react/src/index.js
+++ fl11_20_homework_react/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-<div>
-    <h1>My Todo List</h1>
-    <input placeholder="search"/>
+const TodoList = () =>{
+    return (
     <ul>
         <li>Learn React</li>
         <li>Build Awesome App</li>
     </ul>
+    );
+};
+const AppHeader = () =>{
+    return  <h1>My Todo List</h1>
+};
+
+const SearchInput = () =>{
+    return <input placeholder="search"/>
+}
+
+const App = () =>{
+    return(
+        <div>
+    <AppHeader />
+    <SearchInput />
+    <TodoList />
 </div>
     );
-console.log(el)
-ReactDOM.render(el,document.getElementById('root'));
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
