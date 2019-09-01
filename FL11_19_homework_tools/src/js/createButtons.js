97e9e316741d7cf2ac2de4c3709435b07f2a97a0
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-01 16:37:54 +0300

    Final-Version 3.0

--- FL11_19_homework_tools/src/js/createButtons.js
+++ FL11_19_homework_tools/src/js/createButtons.js
@@ -1,26 +1,25 @@
 const buttonContainer = document.getElementById('battle_buttons');
 
 let paper = document.createElement('button');
-paper.setAttribute('class','button');
-paper.setAttribute('id','papper-button');
+paper.setAttribute('class', 'button');
+paper.setAttribute('id', 'papper-button');
 paper.innerHTML = `<img src='./img/paper.png' alt='paper'>`;
 buttonContainer.appendChild(paper);
 
 let rock = document.createElement('button');
-rock.setAttribute('class','button rock-button');
-rock.setAttribute('id','rock-button');
-rock.innerHTML =  `<img src='./img/Rock.png' alt='rock'>`;
+rock.setAttribute('class', 'button rock-button');
+rock.setAttribute('id', 'rock-button');
+rock.innerHTML = `<img src='./img/Rock.png' alt='rock'>`;
 buttonContainer.appendChild(rock);
 
 let scisors = document.createElement('button');
-scisors.setAttribute('class','button scisors-button');
-scisors.setAttribute('id','scisors-button');
-scisors.innerHTML =  `<img src='./img/cutting.png' alt='cutting'>`;
+scisors.setAttribute('class', 'button scisors-button');
+scisors.setAttribute('id', 'scisors-button');
+scisors.innerHTML = `<img src='./img/cutting.png' alt='cutting'>`;
 buttonContainer.appendChild(scisors);
 
 let reset = document.createElement('button');
-reset.setAttribute('class','button reset-button');
-reset.setAttribute('id','reset-button');
-reset.innerHTML =  `<img src='./img/reset.png' alt='reset'>`;
+reset.setAttribute('class', 'button reset-button');
+reset.setAttribute('id', 'reset-button');
+reset.innerHTML = `<img src='./img/reset.png' alt='reset'>`;
 buttonContainer.appendChild(reset);
-
--- FL11_19_homework_tools/webpack.config.js
+++ FL11_19_homework_tools/webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
