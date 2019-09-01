02f95b9da7833334f820621496a0f761a1295c20
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-02 00:25:28 +0300

    Get started

---
+++ fl11_20_homework_react/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+<div>
+    <h1>My Todo List</h1>
+    <input placeholder="search"/>
+    <ul>
+        <li>Learn React</li>
+        <li>Build Awesome App</li>
+    </ul>
+</div>
+    );
+console.log(el)
+ReactDOM.render(el,document.getElementById('root'));
