fcfcce542940998d4243dcf28784b23825039a5e
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-01 15:05:23 +0300

    Final-Version 1.0

--- FL11_19_homework_tools/src/js/gameFunc.js
+++ FL11_19_homework_tools/src/js/gameFunc.js
@@ -1,62 +1,91 @@
 let btnPaper = document.getElementById('papper-button');
 let btnRock = document.getElementById('rock-button');
 let btnScisor = document.getElementById('scisors-button')
-let battleField1  = document.getElementById('field1');
+let battleField1 = document.getElementById('field1');
 let battleField2 = document.getElementById('field2');
 let battleLog = document.getElementById('battlelog');
+let btnReset = document.getElementById('reset-button');
 let choiceStack = ['paper', 'rock', 'scissors'];
+let checkTry = 0;
 
-btnPaper.addEventListener('click',function(){
+btnReset.addEventListener('click',function(){
+    checkTry=0;
+    battleField1.innerHTML = '';
+    battleField2.innerHTML = '';
+    battleLog.innerHTML='';
+})
+btnPaper.addEventListener('click', function () {
     console.log(choiceStack[0]);
     let userChoice = choiceStack[0];
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice = choiceStack[randomNum];
-    battleField2.innerHTML = `${computerChoice}`;
-    battleField1.innerHTML = `Paper`;
-    checkResult(userChoice,computerChoice);
+    battleField2.innerHTML = `<p class='res'>${computerChoice}</p>`;
+    battleField1.innerHTML = `<p class='res'>Paper</p>`;
+    checkResult(userChoice, computerChoice);
 });
 
-btnRock.addEventListener('click',function(){
+btnRock.addEventListener('click', function () {
     console.log(choiceStack[1]);
     let userChoice = choiceStack[1];
-    battleField1.innerHTML = `Rock`;
+    battleField1.innerHTML = `<p class='res'>Rock</p>`;
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice = choiceStack[randomNum];
-    battleField2.innerHTML = `${computerChoice}`;
-    checkResult(userChoice,computerChoice)
+    battleField2.innerHTML = `<p class='res'>${computerChoice}</p>`;
+    checkResult(userChoice, computerChoice)
 });
 
-btnScisor.addEventListener('click',function(){
+btnScisor.addEventListener('click', function () {
     console.log(choiceStack[2]);
     let userChoice = choiceStack[2];
-    battleField1.innerHTML = `Scissor`;
+    battleField1.innerHTML = `<p class='res'>Scissor<p/>`;
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice = choiceStack[randomNum];
-    battleField2.innerHTML = `${computerChoice}`;
-    checkResult(userChoice,computerChoice)
+    battleField2.innerHTML = `<p class='res'>${computerChoice}</p>`;
+    checkResult(userChoice, computerChoice)
 });
 
-function checkResult(userChoice,computerChoice){
-    if (userChoice === computerChoice){
-        battleLog.innerHTML =  `Oh!!You Tie!`
-        }else if (userChoice === 'paper' && computerChoice === 'rock'){
-        battleLog.innerHTML = `You win!${userChoice} better ${computerChoice}`;
-        console.log("You win!");
-        }else if (userChoice === 'rock' && computerChoice === 'scissors'){
-            battleLog.innerHTML = `You win!${userChoice} better ${computerChoice}`;
-        console.log("You win!");
-        }else if (userChoice === 'scissors' && computerChoice === 'rock'){
-        battleLog.innerHTML = `You lose!${computerChoice} better ${userChoice}`;
-        console.log("You lose!");
-        }else if (userChoice === 'rock' && computerChoice === 'paper'){
-            battleLog.innerHTML = `You lose!${computerChoice} better ${userChoice}`;
-        console.log("You lose!");
-        }else if (userChoice === 'paper' && computerChoice === 'scissors'){
-            battleLog.innerHTML = `You lose!${computerChoice} better ${userChoice}`;
-        console.log("You lose!");
-        }else if (userChoice === 'scissors' || computerChoice === 'paper'){
-            battleLog.innerHTML = `You win!${userChoice} better ${computerChoice}`;
-        console.log("You win!");
+function checkResult(userChoice, computerChoice) {
+    if(checkTry < 3){
+        if (userChoice === computerChoice) {
+            battleLog.innerHTML = `<p class='res'>Oh!!You Tie!</p>`;
+            checkTry+=1;
+        } else if (userChoice === 'paper' && computerChoice === 'rock') {
+            battleLog.innerHTML = `<p class='res'>You win!${userChoice} better ${computerChoice}</p>`;
+            checkTry+=1;
+            console.log("You win!");
+        } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+            battleLog.innerHTML = `<p class='res'>You win!${userChoice} better ${computerChoice}</p>`;
+            checkTry+=1;
+            console.log("You win!");
+        } else if (userChoice === 'scissors' && computerChoice === 'rock') {
+            battleLog.innerHTML = `<p class='res'>You lose!${computerChoice} better ${userChoice}</p>`;
+            checkTry += 1;
+            console.log("You lose!");
+        } else if (userChoice === 'rock' && computerChoice === 'paper') {
+            battleLog.innerHTML = `<p class='res'>You lose!${computerChoice} better ${userChoice}</p>`;
+            checkTry += 1;
+            console.log("You lose!");
+        } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+            battleLog.innerHTML = `<p class='res'>You lose!${computerChoice} better ${userChoice}</p>`;
+            checkTry += 1;
+            console.log("You lose!");
+        } else if (userChoice === 'scissors' || computerChoice === 'paper') {
+            battleLog.innerHTML = `<p class='res'>You win!${userChoice} better ${computerChoice}</p>`;
+            checkTry += 1;
+            console.log("You win!");
         }
+    }else{
+        alert('Sorry Game Ends');
+        // doYouWannaPlay();
+    }
 }
+// function doYouWannaPlay(){
+//     let question = confirm("Do you wanna play more?");
+//     if(question == true){
+//        alert("Okey Let's go again");
+//        checkTry = 0;
+//     }else if(question == false){
+//         alert('Why you sooo boring....');
+//     }
 
+// }
