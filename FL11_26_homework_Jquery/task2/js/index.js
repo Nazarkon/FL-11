5fab804562ba14efadcbacdf2a0524b8f8765a27
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-24 20:00:50 +0300

    Final

--- FL11_26_homework_Jquery/task2/js/index.js
+++ FL11_26_homework_Jquery/task2/js/index.js
@@ -16,27 +16,33 @@ const todos = [{
 
 $(document).ready(
   function () {
+    $list.html(localStorage.getItem('ItemList',$list.html()))
     $($add).click(function () {
       let newItem = $input.val();
-      let newLi = $('<li class="item">' + '<span class="item-text">' + newItem + '</span>' + '<button class="item-remove">Remove</button>' + '</li>');
-      $list.append(newLi);
-      $input.val('');
+      if(newItem){
+        let newLi = $('<li class="item">' + '<span class="item-text">' + newItem + '</span>' + '<button class="item-remove">Remove</button>' + '</li>');
+        $list.append(newLi);
+        $input.val('');
+        localStorage.setItem('ItemList',$list.html());
+      }else{
+      return false;
+      }
+    })
+    localStorage.setItem('ItemList',$list.html());
+  });
 
-      $remBtn.on('click', function () {
-        let $parent = $(this).parent();
-        console.log($parent)
-        $parent.remove();
-      })
-
-      $('.item').on('click', function () {
-        let $parent = $(this).children();
+  $(document).on('click','.item',function(){
+    let $parent = $(this).children();
         if ($parent.hasClass('done')) 
         {
           $parent.removeClass('done');
         } else {
           $parent.addClass('done');
         }
-      })
-      return false;
-    })
+  });
+
+  $(document).on('click', '.item-remove', function() {
+    let $parent = $(this).parent();
+    $parent.remove();
+    localStorage.setItem('ItemList',$list.html());
   });
